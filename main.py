38d7506a124e70456414251da48edfeb7c1b03a7
Jade Streeter <131019325+jstreeter818@users.noreply.github.com>
2023-05-01 13:19:15 -0500

    added function to check for wins

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def drop_piece(board, row, col, piece):
 
 
 def is_valid_location(board, col):
-    return board[5][col] == 0
+    return board[row_count - 1][col] == 0
 
 
 def get_next_open_row(board, col):
@@ -30,10 +30,34 @@ def print_board(board):
     print(np.flip(board, 0))
 
 
+def check_for_win(board, piece):
+    # Check horizontal wins
+    for c in range(column_count - 3):
+        for r in range(row_count):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+                return True
+    # Check vertical wins
+    for c in range(column_count):
+        for r in range(row_count - 3):
+            if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+                return True
+    # Check positive diagonal wins
+    for c in range(column_count - 3):
+        for r in range(row_count - 3):
+            if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+                return True
+    # Check negative diagonal wins
+    for c in range(column_count - 3):
+        for r in range(3, row_count):
+            if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+                return True
+
+
 board = create_board()
 print_board(board)
 game_over = False
 turn = 0
+winner = None
 
 # main game loop
 while not game_over:
@@ -42,14 +66,22 @@ while not game_over:
         if is_valid_location(board, col):
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 1)
+            if check_for_win(board, 1):
+                game_over = True
+                winner = 'Player 1'
 
     else:
         col = int(input("PLayer 2, make your selection(0-6): "))
         if is_valid_location(board, col):
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 2)
+            if check_for_win(board, 2):
+                game_over = True
+                winner = 'Player 2'
 
     print_board(board)
 
     turn += 1
     turn = turn % 2
+
+print(winner, 'won!')
