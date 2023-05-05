e8dc5a4f0a84d2e8633e5897b87741379f3246f2
Jade Streeter <131019325+jstreeter818@users.noreply.github.com>
2023-05-05 14:03:14 -0500

    created gui window elements and added logic + event handlers

--- connect4.py
+++ connect4.py
@@ -1,14 +1,11 @@
-import pygame
 import numpy as np
-import math
-import sys
 
 row_count = 6
 column_count = 7
 
 
 def create_board():
-    board = np.zeros((6,7))
+    board = np.zeros((6, 7))
     return board
 
 
@@ -24,10 +21,6 @@ def get_next_open_row(board, col):
     for i in range(row_count):
         if board[i][col] == 0:
             return i
-
-
-def print_board(board):
-    print(np.flip(board, 0))
 
 
 def check_for_win(board, piece):
@@ -51,37 +44,3 @@ def check_for_win(board, piece):
         for r in range(3, row_count):
             if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
                 return True
-
-
-board = create_board()
-print_board(board)
-game_over = False
-turn = 0
-winner = None
-
-# main game loop
-while not game_over:
-    if turn == 0:
-        col = int(input("PLayer 1, make your selection(0-6): "))
-        if is_valid_location(board, col):
-            row = get_next_open_row(board, col)
-            drop_piece(board, row, col, 1)
-            if check_for_win(board, 1):
-                game_over = True
-                winner = 'Player 1'
-
-    else:
-        col = int(input("PLayer 2, make your selection(0-6): "))
-        if is_valid_location(board, col):
-            row = get_next_open_row(board, col)
-            drop_piece(board, row, col, 2)
-            if check_for_win(board, 2):
-                game_over = True
-                winner = 'Player 2'
-
-    print_board(board)
-
-    turn += 1
-    turn = turn % 2
-
-print(winner, 'won!')
---
+++ gameWindow.py
@@ -0,0 +1,104 @@
+import pygame
+import connect4
+import sys
+import math
+
+
+def main():
+    def draw_board(board):
+        for c in range(connect4.column_count):
+            for r in range(connect4.row_count):
+                pygame.draw.rect(screen, blue, (c * squaresize, r * squaresize + squaresize, squaresize, squaresize))
+                pygame.draw.circle(screen, black, (int(c * squaresize + squaresize / 2), int(r * squaresize + squaresize + squaresize / 2)), radius)
+
+        for c in range(connect4.column_count):
+            for r in range(connect4.row_count):
+                if board[r][c] == 1:
+                    pygame.draw.circle(screen, red, (int(c * squaresize + squaresize / 2), height - int(r * squaresize + squaresize / 2)), radius)
+                elif board[r][c] == 2:
+                    pygame.draw.circle(screen, yellow, (int(c * squaresize + squaresize / 2), height - int(r * squaresize + squaresize / 2)), radius)
+
+        pygame.display.update()
+
+    red = (186, 66, 58)
+    blue = (95, 130, 194)
+    black = (0, 0, 0)
+    yellow = (207, 190, 107)
+
+    board = connect4.create_board()
+    game_over = False
+    turn = 0
+
+    pygame.init()
+
+    squaresize = 100
+
+    width = connect4.column_count * squaresize
+    height = (connect4.row_count+1) * squaresize
+
+    size = (width, height)
+    radius = int(squaresize/2 - 5)
+
+    screen = pygame.display.set_mode(size)
+
+    draw_board(board)
+    pygame.display.set_caption('Connect 4!')
+    pygame.display.update()
+
+    myfont = pygame.font.SysFont("monospace", 75)
+
+    # Window loop
+    while not game_over:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            if event.type == pygame.MOUSEMOTION:
+                pygame.draw.rect(screen, black, (0,0, width, squaresize))
+                posx = event.pos[0]
+                if turn == 0:
+                    pygame.draw.circle(screen, red, (posx, int(squaresize/2)), radius)
+                else:
+                    pygame.draw.circle(screen, yellow, (posx, int(squaresize/2)), radius)
+            pygame.display.update()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pygame.draw.rect(screen, black, (0,0, width, squaresize))
+                if turn == 0:
+                    posx = event.pos[0]
+                    col = int(math.floor(posx/squaresize))
+
+                    if connect4.is_valid_location(board, col):
+                        row = connect4.get_next_open_row(board, col)
+                        connect4.drop_piece(board, row, col, 1)
+
+                        if connect4.check_for_win(board, 1):
+                            label = myfont.render('Player 1 wins!', 1, red)
+                            screen.blit(label, (40, 10))
+                            game_over = True
+
+                else:
+                    posx = event.pos[0]
+                    col = int(math.floor(posx/squaresize))
+
+                    if connect4.is_valid_location(board, col):
+                        row = connect4.get_next_open_row(board, col)
+                        connect4.drop_piece(board, row, col, 2)
+
+                        if connect4.check_for_win(board, 2):
+                            label = myfont.render('Player 2 wins!', 1, yellow)
+                            screen.blit(label, (40, 10))
+                            game_over = True
+
+                draw_board(board)
+
+                turn += 1
+                turn = turn % 2
+
+                if game_over:
+                    pygame.time.wait(3000)
+
+
+if __name__ == '__main__':
+    main()
